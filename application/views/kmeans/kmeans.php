e4c5fc2bbd4a1756778925d9edc1135b1be1aa88
jclavota <jclavota@TRJ-GRD01G2.usersad.everis.int>
2019-03-11 14:41:53 -0500

    Create Navs

--- application/views/kmeans/kmeans.php
+++ application/views/kmeans/kmeans.php
@@ -9,7 +9,7 @@
 	rel="stylesheet">
 </head>
 <body>
-		<div class="container">
+	<div class="container">
 		<div class="row">
 			<!-- FORM  -->
 			<div class="col-md-12">
@@ -17,14 +17,13 @@
 					<fieldset>
 						<!-- Form description -->
 						<legend>Kmeans execution</legend>
-					
+
 						<!-- Select  -->
 						<div class="form-group">
 							<label class="col-md-4 control-label" for="kmeans-test">tests</label>
 							<div class="col-md-4">
 								<div class="input-group">
-									<select 
-										class="form-control" id="kmeans-test">
+									<select class="form-control" id="kmeans-test">
 									</select>
 								</div>
 							</div>
@@ -40,62 +39,86 @@
 						<button id="btn-save" name="btn-save" class="btn btn-success"
 							onclick="execute_kmeans()">Execute Kmeans</button>
 					</div>
-                </div>
+				</div>
 			</div>
+
+			<!-- Nav tabs -->
+			<div class="col-md-12">
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="nav-item"><a class="nav-link active" data-toggle="tab"
+						href="#clusters">Clusters</a></li>
+					<li class="nav-item"><a class="nav-link" data-toggle="tab"
+						href="#students">Students</a></li>
+					<li class="nav-item"><a class="nav-link" data-toggle="tab"
+						href="#histograms">Histograms</a></li>
+				</ul>
+			</div>
+
 
 			<!-- LIST CLUSTERS -->
-			<div class=col-md-12>
-				Clusters
-				<table class="table table-bordered table-condensed table-hover">
-					<thead>
-						<tr>
-							<th>Value</th>
-							<th>Letter</th>
-						</tr>
+			<div class="tab-content">
+				<div id="clusters" class="tab-pane active">
+					<br>
+					<div class="col-md-12">
+						Clusters
+						<table class="table table-bordered table-condensed table-hover">
+							<thead>
+								<tr>
+									<th>Value</th>
+									<th>Letter</th>
+								</tr>
 
-					</thead>
-					<tbody id="form-list-kmeans-clusters">
+							</thead>
+							<tbody id="form-list-kmeans-clusters">
 
-					</tbody>
-				</table>
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+				<!-- LIST STUDENTS -->
+				<div id="students" class="container tab-pane fade">
+					<br>
+					<div class=col-md-12>
+						Students
+						<table class="table table-bordered table-condensed table-hover">
+							<thead>
+								<tr>
+									<th>id</th>
+									<th>average</th>
+									<th>cluster</th>
+									<th>cluster value</th>
+								</tr>
+
+							</thead>
+							<tbody id="form-list-kmeans-students">
+
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+				<!-- LIST HISTOGRAMS -->
+				<div id="histograms" class="container tab-pane fade">
+					<br>
+					<div class=col-md-12>
+						Histograms
+						<table class="table table-bordered table-condensed table-hover">
+							<thead>
+								<tr id="form-list-kmeans-histograms-header">
+									<th>question id</th>
+									<th>question</th>
+								</tr>
+
+							</thead>
+							<tbody id="form-list-kmeans-histograms-body">
+
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
-			
-			<!-- LIST STUDENTS -->
-			<div class=col-md-12>
-				Students
-				<table class="table table-bordered table-condensed table-hover">
-					<thead>
-						<tr>
-							<th>id</th>
-							<th>average</th>
-							<th>cluster</th>
-							<th>cluster value</th>
-						</tr>
-
-					</thead>
-					<tbody id="form-list-kmeans-students">
-
-					</tbody>
-				</table>
-			</div>
-			
-			<!-- LIST HISTOGRAMS -->
-			<div class=col-md-12>
-				Histograms
-				<table class="table table-bordered table-condensed table-hover">
-					<thead >
-						<tr id="form-list-kmeans-histograms-header">
-							<th>question id</th>
-							<th>question</th>
-						</tr>
-
-					</thead>
-					<tbody id="form-list-kmeans-histograms-body">
-
-					</tbody>
-				</table>
-			</div>
-			<br> 
+			<br>
 			<!-- view -->
 		</div>
 	</div>
